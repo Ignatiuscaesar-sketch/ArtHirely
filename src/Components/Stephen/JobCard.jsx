f1f8bc46e25a485e87cf5e7304e4d26f24f93ef6
Stephen Onyango <onyangosteve01@gmail.com>
2024-05-07 18:06:11 +0300

    Adds basic jsx to file

--- src/Components/Stephen/JobCard.jsx
+++ src/Components/Stephen/JobCard.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
-function JobList() {
+function JobCard() {
   return (
-    <div>JobCard</div>
-  )
+    <>
+      <div className="main-button-container">
+        {/* top buttons start here */}
+        <div class="button-designs first-section">
+          <div>
+            <button class="button-designs third-button" id="freelance">
+              Freelance
+            </button>
+          </div>
+          <div>
+            <button class="button-designs second-button" id="Full-Time">
+              Full-Time
+            </button>
+          </div>
+          <div>
+            <button class="button-designs third-button" id="creatives-for-hire">
+              Creatives for hire
+            </button>
+          </div>
+        </div>
+        {/* lower buttons start here */}
+
+        <div className="button-designs lower-section">
+          <div>
+            <h6 className="button-designs" id="freelance-jobs">
+              <span>300</span> Freelance Jobs
+            </h6>
+          </div>
+          <div>
+            <button className="button-designs" id="blue-button">
+              Post Jobs
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default JobList
+export default JobCard;
