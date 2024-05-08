eb7296815a61bf395b0432837be84d38d8423c27
Stephen Onyango <onyangosteve01@gmail.com>
2024-05-08 19:21:57 +0300

    test server

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
+import JobTab from './Components/Stephen/JobTab';
 
-function Header() {
+function App() {
     return (
-        <header>
-            <div className="logo">ArtHirely</div>
-            <nav>
-                <ul>
-                    <li>For You</li>
-                    <li>Discover</li>
-                    <li>Hire</li>
-                    <li>Contacts</li>
-                    <li>About Us</li>
-                </ul>
-            </nav>
-            <div className="actions">
-                <button className="login">Log In</button>
-                <button className="signup">Sign Up</button>
-            </div>
-        </header>
+        <div>
+            <JobTab />
+        </div>
     );
 }
 
-export default Header;
+export default App;
