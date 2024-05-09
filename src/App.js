13c23e28cfd2a756421f8bc9183eaab0dd164655
Stephen Onyango <onyangosteve01@gmail.com>
2024-05-09 10:32:30 +0300

    Adds extra changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import './Header.css'; // Assume CSS is already written
+import Server from './Components/Stephen/Server';
 
 function App() {
     return (
-        <header>
-            <div className="logo">ArtHirely</div>
-            <nav>
-                <ul>
-                    <li>For You</li>
-                    <li>Discover</li>
-                    <li>Hire</li>
-                    <li>Contacts</li>
-                    <li>About Us</li>
-                </ul>
-            </nav>
-            <div className="actions">
-                <button className="login">Log In</button>
-                <button className="signup">Sign Up</button>
-            </div>
-        </header>
+        <>
+        <Server />
+        </>
     );
 }
 
