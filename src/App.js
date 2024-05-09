3f644a76379aa1741dfd1469257dc7949365bebb
Fardosafara <firdausgedi2@gmail.com>
2024-05-09 22:57:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Main from './Components/FardosaGedi/Main';
 function App() {
     return (
       <div>
-        <h1>Hi</h1>
+     <h1>I was assigned to a folder in inside steven Components due to unforeseen technical issues.
+This measure was taken to ensure my work could continue seamlessly and without interruption.</h1> 
         <Main />
       </div>
     );
