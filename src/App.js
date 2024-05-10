7d5985bc4c8fd4bf033da9c4c122ebf9136ff718
Fardosafara <firdausgedi2@gmail.com>
2024-05-10 18:21:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,25 +3,12 @@ import Main from './Components/FardosaGedi/Main';
 
 function App() {
     return (
-        <header>
-            <div className="logo">ArtHirely</div>
-            <nav>
-                <ul>
-                    <li>For You</li>
-                    <li>Discover</li>
-                    <li>Hire</li>
-                    <li>Contacts</li>
-                    <li>About Us</li>
-                    <li>Welcome</li>
-                    <li>Hello</li>
-                </ul>
-            </nav>
-            <div className="actions">
-                <button className="login">Log In</button>
-                <button className="signup">Sign Up</button>
-            </div>
-        </header>
-    );
-}
-
+        <div className="App">
+        <Main />
+        </div>
+        );
+     }
+        
+   
+    
 export default App;
