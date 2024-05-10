2f4725e886f3f2f84abc8f5aaf52391c889b2002
Fardosafara <firdausgedi2@gmail.com>
2024-05-11 02:12:35 +0300

    Resolve conflicts

--- src/Components/FardosaGedi/Server.js
+++ src/Components/FardosaGedi/Server.js
@@ -56,7 +56,7 @@ function Server() {
 
     return (
       <form className="formContainer" onSubmit={handleJobSubmit}>
-        <h2>Job Posting Form</h2>
+        <h2>Job Posting</h2>
         <label>
             Time:
             <input type="text" name="time" value={formData.time} onChange={handleChange} required />
