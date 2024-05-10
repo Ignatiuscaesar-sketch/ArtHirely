cda18275c64b0821937232597971fb99c406c2f6
James <jamonjoroge816@gmail.com>
2024-05-10 21:48:10 +0300

    INITIAL Commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { Link ,useLocation} from 'react-router-dom';
 import LoginForm from './LoginForm';
@@ -12,14 +13,18 @@ function Header() {
   return (
     <div className='header'>
       <div className="header-left">
-        <h2>ArtHirely</h2>
+        <h2>
+        <span style={{ color: 'black' }}> Art</span>
+        <span style={{ color: 'blue' }}>Hirely</span>
+        </h2>
       </div>
 
       <div className="header-center">
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
-          <li><a href="#">Jobs</a></li>
+          <li><Link to="/jobs">Jobs</Link></li>
+
         </ul>
       </div>
 
